06b55878a65dcef6e6ac412f1122127c7d1287bf
Harshavardhan Patekar <66788476+hupatekar17@users.noreply.github.com>
2024-05-13 18:02:44 +0530

    final commit
    Added all the functionality.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
-
 import "./App.css";
-import { Route, Routes, Navigate, useLocation} from "react-router-dom";
-import Home from "./components/home";
-import AboutUs from "./components/about";
-import Users from "./components/users"
-import Navbar from "./components/navbar"
-import NotFound from "./components/notfound";
-import UserProfile from "./components/userProfile";
-import SearchUser from "./components/searchUser";
-import Login from "./components/login";
-import AuthProfile from "./components/authProfile"
-import { useState } from "react";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import React, { useState, Suspense } from "react";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
+import { appRoutes } from "./routes";
 
 
 function App() {
   const [username, setUsername] = useState("");
   const [isLogged, setIsLogged] = useState(false);
   const location = useLocation();
-
   return (
-    <SwitchTransition component={null}> 
-      <CSSTransition key={location.pathname} classNames="fade" timeout={300} unmountOnExit>
- 
-      <Routes location={location }>
-         <Route exact path="/" element={<Home />} /> 
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/users" element={<Users/>} />
-          <Route element={<UserProfile/>} path="/users/user/:username"/>
-          <Route element={<SearchUser/>} path="/search"/>
-          <Route element={<Login setIsLogged={setIsLogged} setUsername={setUsername}/>} path="/login"/>
-
-          <Route element={isLogged ?( <AuthProfile username={username}/> ): (<Navigate replace to ={"/login"}/>)} path="/authProfile"/>
-         
-
-
-        <Route path="*" element={<NotFound/>}/>;
-      </Routes> 
-
-
+    <SwitchTransition component={null}>
+      <CSSTransition
+        key={location.pathname}
+        classNames="fade"
+        timeout={300}
+        unmountOnExit
+      >
+        <Suspense fallback={() => <h1>Loading...</h1>}>
+          <Routes location={location}>
+            {appRoutes.map((route) => {
+              if (route.requiresAuth && !isLogged) {
+                return (
+                  <Route
+                    key={route.path}
+                    exact
+                    path={route.path}
+                    element={<Navigate replace to={"/login"} />}
+                  />
+                );
+              } else {
+                return (
+                  <Route
+                    key={route.path}
+                    exact
+                    path={route.path}
+                    element={
+                      <route.component
+                        setIsLogged={setIsLogged}
+                        setUsername={setUsername}
+                        username={username}
+                      />
+                    }
+                  />
+                );
+              }
+            })}
+          </Routes>
+        </Suspense>
       </CSSTransition>
-
     </SwitchTransition>
-    
-     
-  
   );
 }
 
